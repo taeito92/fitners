892ea5d25302c5e588032995c3a83789afa65b59
taeito92 <bethest4r@gmail.com>
2021-09-27 09:50:56 +0900

    asd

--- src/main/java/org/dodream/web/fitners/auction/mapper/TimeMapper.java
+++ src/main/java/org/dodream/web/fitners/auction/mapper/TimeMapper.java
@@ -3,8 +3,8 @@ package org.dodream.web.fitners.auction.mapper;
 import org.apache.ibatis.annotations.Select;
 
 public interface TimeMapper {
+    String selectTime();
 
     @Select("select now()")
-    String getTime();
-
+    String getTime2();
 }
