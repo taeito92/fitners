771812b9b694e134c226392fc3805e5b05d0544a
taeito92 <bethest4r@gmail.com>
2021-09-28 12:36:30 +0900

    asd

--- src/main/java/org/dodream/web/fitners/common/dto/PageRequestDTO.java
+++ src/main/java/org/dodream/web/fitners/common/dto/PageRequestDTO.java
@@ -17,9 +17,23 @@ public class PageRequestDTO {
     @Builder.Default
     private int size = 10;
 
+    private String type;
+    private String keyword;
+
     public int getSkip() {
         return (page - 1) * size;
     }
+
+    public String getType() { //sql if 조건을위해 type이 존재하는지 빈란인지 식별한
+        if (type == null || type.trim().length() == 0) { //trim은 공백을 제거하는 메서드
+            return null;
+        }
+        return this.type;
+    }
+
+    public String[] getArr() {
+        return type == null? new String[]{} : type.split("");
+    } //split 기준으로 자르고 문자열로 저장(검색 좀 더 해볼 것)
 
 
 }
